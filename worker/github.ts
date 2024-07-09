d1f4e9a8d4d7c929dbe3bf927988fe18a79ca51c
manterfield <tom@hiphops.io>
2024-07-09 14:29:18 +0100

    WIP - Debugging callHandler

--- worker/github.ts
+++ worker/github.ts
@@ -1,11 +1,12 @@
-import {
-  Octokit,
-  type Octokit as OctokitT,
-} from "https://esm.sh/octokit@4.0.2?dts";
+// import {
+//   Octokit,
+//   type Octokit as OctokitT,
+// } from "https://esm.sh/octokit@4.0.2?dts";
 import { call } from "./functions.ts";
 
 /** github returns a pre-authenticated Octokit instance */
-export const github = async (): Promise<OctokitT> => {
+export const github = async () => {
+  const { Octokit } = await import("https://esm.sh/octokit@4.0.2?dts");
   const token: string = (await call("hiphops.github.accesstoken")) as string;
   return new Octokit({ auth: token });
 };
