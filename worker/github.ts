8376a9bf1d3d02cc4041b43c5276451877ca8995
manterfield <tom@hiphops.io>
2024-07-09 10:09:53 +0100

    Removed debug logging

--- worker/github.ts
+++ worker/github.ts
@@ -4,10 +4,8 @@ import {
 } from "https://esm.sh/octokit@4.0.2?dts";
 import { call } from "./functions.ts";
 
+/** github returns a pre-authenticated Octokit instance */
 export const github = async (): Promise<OctokitT> => {
   const token: string = (await call("hiphops.github.accesstoken")) as string;
-
-  console.log("---lib token:", token);
-
   return new Octokit({ auth: token });
 };
