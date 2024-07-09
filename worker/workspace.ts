ece840f46893b2fd71389cbbd39e738e1ca835c7
manterfield <tom@hiphops.io>
2024-07-09 18:55:53 +0100

    WIP - Git checkout

--- worker/workspace.ts
+++ worker/workspace.ts
@@ -1,12 +1,17 @@
 export const workspace = () => {
+  console.log("Creating workspace dir ", WORKSPACE_DIR);
   // TODO: WORKSPACE_DIR shouldn't be a global var given we're doing JIT creation
   try {
     Deno.mkdirSync(WORKSPACE_DIR, {});
   } catch (err) {
     if (err! instanceof Deno.errors.AlreadyExists) {
       throw err;
+    } else {
+      console.log("Dir already exists");
     }
   }
+
+  console.log("Workspace dir contents ", Deno.readDir(WORKSPACE_DIR));
 
   return WORKSPACE_DIR;
 };
