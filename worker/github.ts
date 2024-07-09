8807b3fbcd140ff63df8bdf0d28d7c364bd67016
manterfield <tom@hiphops.io>
2024-07-09 14:54:13 +0100

    WIP - Debugging callHandler

--- worker/github.ts
+++ worker/github.ts
@@ -1,8 +1,16 @@
+// import {
+//   Octokit,
+//   type Octokit as OctokitT,
+// } from "https://esm.sh/octokit@4.0.2?dts";
 import { call } from "./functions.ts";
 
 /** github returns a pre-authenticated Octokit instance */
 export const github = async () => {
-  const { Octokit } = await import("https://esm.sh/octokit@4.0.2?dts");
+  // Dynamic import is split to prevent Deno static analysis pre-downloading
+  // the module even if it's not needed.
+  const octoImport = "https://esm.sh/octokit@4.0.2?dts";
+  const { Octokit } = await import(octoImport);
+
   const token: string = (await call("hiphops.github.accesstoken")) as string;
   return new Octokit({ auth: token });
 };
