83cb68680f6e3702c1450660bf9209bcf4caab62
manterfield <tom@hiphops.io>
2024-07-15 15:00:53 +0100

    WIP - Slack client library

--- worker/slack.ts
+++ worker/slack.ts
@@ -2,17 +2,11 @@ import { SlackAPI } from "https://deno.land/x/deno_slack_api@2.1.1/mod.ts";
 
 import { call } from "./functions.ts";
 
-export const client = async (channelId: string) => {
+export const client = async () => {
   console.log("Getting slack client token");
   const token: string = (await call("hiphops.slack.accesstoken")) as string;
 
   console.log("Getting slack API client");
-  const client = SlackAPI(token);
 
-  await client.chat.postMessage({
-    text: "Hello from client",
-    channel: channelId,
-  });
-  console.log("Message sent, returning client");
-  return { client, msg: "hey" };
+  return { client: SlackAPI(token), msg: "hey" };
 };
