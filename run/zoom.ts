6519df06127276ed7e06a51d7f57d4107cfe43fb
Lukas Oberhuber <lukas@hiphops.io>
2024-08-15 18:34:28 +0100

    Fix Zoom access_token getting

--- run/zoom.ts
+++ run/zoom.ts
@@ -2,21 +2,17 @@ import ky, { type KyInstance } from "https://esm.sh/ky@1.4.0";
 
 import { call } from "./functions.ts";
 
-export type ZoomCredentials = {
-  access_token: string;
-};
-
-export const auth = async (): Promise<ZoomCredentials> => {
-  return (await call("hiphops.zoom.accesstoken")) as ZoomCredentials;
+export const auth = async (): Promise<string> => {
+  return (await call("hiphops.zoom.accesstoken")) as string;
 };
 
 export const client = async (): Promise<KyInstance> => {
-  const { access_token } = await auth();
+  const token: string = await auth();
 
   return ky.extend({
     prefixUrl: `https://api.zoom.us/v2`,
     headers: {
-      Authorization: `Bearer ${access_token}`,
+      Authorization: `Bearer ${token}`,
     },
   });
 };
